ca098ae7eea86903d2b523715580c6f4ac7a2ba0
zan-d <53127723+zan-d@users.noreply.github.com>
2022-04-01 16:57:43 -0400

    serial communication works both ways

--- python_code/home.py
+++ python_code/home.py
@@ -4,6 +4,7 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.screenmanager import Screen
 import serial
+from time import sleep
 
 Builder.load_file("home.kv")
 
@@ -20,12 +21,21 @@ class HomeScreen(Screen):
         self.water_button = 0
         self.light_button = 0
         self.heat_button = 0
-        Clock.schedule_interval(self.update_labels, 5)
+
+        self.com = 'COM8'
+        self.baudrate = 9600
+        self.ser = serial.Serial(self.com, self.baudrate)
+        sleep(2)
+        Clock.schedule_interval(self.update_labels, 10)
 
     def plant_details(self):
         if self.app.current_plant != "Please select plant":
             # serial write ideal plant properties
-            # ser.write(b"D,\n")
+            # with serial.Serial(self.com, self.baudrate) as ser:
+            status = "D,{},{},{},{},{}\n".format(self.app.current_temp_l, self.app.current_temp_h, self.app.current_light_h,
+                                               self.app.current_moist_l, self.app.current_moist_h)
+            self.ser.write(status.encode('ascii'))
+
             print("D,{},{},{},{},{}".format(self.app.current_temp_l, self.app.current_temp_h, self.app.current_light_h,
                                             self.app.current_moist_l, self.app.current_moist_h))
             self.ids.selected_plant.text = "Ideal temperature range: {} - {} (Celcius)\n" \
@@ -54,19 +64,31 @@ class HomeScreen(Screen):
         elif button_name == "fan":
             self.fan_button = state
 
-        print("Water: {} Heat: {} Light: {} Fan: {}".format(self.water_button, self.heat_button, self.light_button,
-                                                            self.fan_button))
+        # print("Water: {} Heat: {} Light: {} Fan: {}".format(self.water_button, self.heat_button, self.light_button,
+        #                                                     self.fan_button))
+        print("S,{},{},{},{}".format(self.water_button, self.heat_button, self.light_button, self.fan_button))
+
         # serial write button states
-        # ser.write(b"S,\n")
+        # ser = serial.Serial(self.com, self.baudrate)
+        # sleep(2)
+        status = "S,{},{},{},{}\n".format(self.water_button, self.heat_button, self.light_button, self.fan_button)
+        self.ser.write(status.encode('ascii'))
 
     def update_labels(self, *args):
-        with serial.Serial('COM8', 9600) as ser:
-            status = ser.readline()
-            while ser.inWaiting() > 0:
-                status = ser.readline()
-            data = status
-            print(data)
-            data = data.strip()
-            ser.close()
-            # self.moist_text, self.temp_text, self.co2_text = data.decode('ascii').split(',')
+        # ser = serial.Serial(self.com, self.baudrate)
+        # sleep(2)
+        # get the most recent data
+        while self.ser.inWaiting() > 0:
+            status = self.ser.readline()
+            status = status.decode('ascii').strip()
+            if "echo" in status:
+                print(status)
+        data = status
+        print(data)
+        if data.count(",") == 2:
+            moist, temp, co2 = data.split(',')
+            self.moist_text = "Moisture: " + moist
+            self.temp_text = "Temperature: " + temp
+            self.co2_text = "CO2: " + co2
+
 
